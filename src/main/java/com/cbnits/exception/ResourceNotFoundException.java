55f8249736f90d6809a42b96869caa51cc8b5391
tapajyotighosh10 <tapajyotighosh10@gmail.com>
2025-01-01 12:45:31 +0530

    removed unused imports

--- src/main/java/com/cbnits/exception/ResourceNotFoundException.java
+++ src/main/java/com/cbnits/exception/ResourceNotFoundException.java
@@ -1,9 +1,6 @@
 package com.cbnits.exception;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
         super(message);
