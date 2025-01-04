c33ab6a4925ecf9ac7f92cde3f948b6d5dad535f
tapajyotighosh10 <tapajyotighosh10@gmail.com>
2025-01-04 23:52:46 +0530

    added lombok dependency

--- src/main/java/com/cbnits/entity/Employee.java
+++ src/main/java/com/cbnits/entity/Employee.java
@@ -9,6 +9,10 @@ import org.springframework.cglib.core.Local;
 import java.time.LocalDateTime;
 @Entity
 @Table(name = "employees_details")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
@@ -28,96 +32,98 @@ public class Employee {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-    public Employee(Long empId, String empName, String email, String mobileNo, String salary, String project, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.empId = empId;
-        this.empName = empName;
-        this.email = email;
-        this.mobileNo = mobileNo;
-        this.salary = salary;
-        this.project = project;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+//    public Employee(Long empId, String empName, String email, String mobileNo, String salary, String project, LocalDateTime createdAt, LocalDateTime updatedAt) {
+//        this.empId = empId;
+//        this.empName = empName;
+//        this.email = email;
+//        this.mobileNo = mobileNo;
+//        this.salary = salary;
+//        this.project = project;
+//        this.createdAt = createdAt;
+//        this.updatedAt = updatedAt;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public LocalDateTime getUpdatedAt() {
+//        return updatedAt;
+//    }
+//
+//    public void setUpdatedAt(LocalDateTime updatedAt) {
+//        this.updatedAt = updatedAt;
+//    }
+//
+//    public Long getEmpId() {
+//        return empId;
+//    }
+//
+//    public void setEmpId(Long empId) {
+//        this.empId = empId;
+//    }
+//
+//    public String getEmpName() {
+//        return empName;
+//    }
+//
+//    public void setEmpName(String empName) {
+//        this.empName = empName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getMobileNo() {
+//        return mobileNo;
+//    }
+//
+//    public void setMobileNo(String mobileNo) {
+//        this.mobileNo = mobileNo;
+//    }
+//
+//    public String getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(String salary) {
+//        this.salary = salary;
+//    }
+//
+//    public String getProject() {
+//        return project;
+//    }
+//
+//    public void setProject(String project) {
+//        this.project = project;
+//    }
+//
+//
+//    public Employee() {
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Employee{" +
+//                "empId=" + empId +
+//                ", empName='" + empName + '\'' +
+//                ", email='" + email + '\'' +
+//                ", mobileNo='" + mobileNo + '\'' +
+//                ", salary='" + salary + '\'' +
+//                ", project='" + project + '\'' +
+//                ", createdAt=" + createdAt +
+//                ", updatedAt=" + updatedAt +
+//                '}';
+//    }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Long getEmpId() {
-        return empId;
-    }
-
-    public void setEmpId(Long empId) {
-        this.empId = empId;
-    }
-
-    public String getEmpName() {
-        return empName;
-    }
-
-    public void setEmpName(String empName) {
-        this.empName = empName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-
-    public Employee() {
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "empId=" + empId +
-                ", empName='" + empName + '\'' +
-                ", email='" + email + '\'' +
-                ", mobileNo='" + mobileNo + '\'' +
-                ", salary='" + salary + '\'' +
-                ", project='" + project + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 }
